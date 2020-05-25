9238c5959b2125b300cdef0826e391c814a2f263
Wellington Della Mura <della.mura@gmail.com>
2020-05-25 09:43:45 -0300

    subscription

--- js/api.js
+++ js/api.js
@@ -2,37 +2,40 @@
 
 const server = "https://fullstack.della-mura.com.br/";
 const subscriptionEndPoint = server + "api/subscription";
-const jsonHeaders = new Headers({'Content-Type': 'application/json', 'Accept': 'application/json'});
+const jsonHeaders = new Headers({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+});
 
-function doPost(url, object, fnOk, fnError){
+function doPost(url, object, fnOk, fnError) {
     fetch(url, {
         method: 'post',
         redirect: 'follow',
         headers: jsonHeaders,
-        body: JSON.stringify(object )
-    }).then(function(response) {
-        response.json().then(function(data){
+        body: JSON.stringify(object)
+    }).then(function (response) {
+        response.json().then(function (data) {
             fnOk(response.status, data);
-        }).catch(function(error) {
+        }).catch(function (error) {
             fnError(response.status, error);
         });
-    }).catch(function(error) {
+    }).catch(function (error) {
         fnError(response.status, error);
     });
 }
 
-function doGet(url, fnOk, fnError){
+function doGet(url, fnOk, fnError) {
     fetch(url, {
         method: 'get',
         redirect: 'follow',
         headers: jsonHeaders,
-    }).then(function(response) {
-        response.json().then(function(data){
+    }).then(function (response) {
+        response.json().then(function (data) {
             fnOk(response.status, data);
-        }).catch(function(error) {
+        }).catch(function (error) {
             fnError(response.status, error);
         });
-    }).catch(function(error) {
+    }).catch(function (error) {
         fnError(response.status, error);
     });
 }
@@ -40,5 +43,5 @@ function doGet(url, fnOk, fnError){
 
 function toApiDate(date) {
     let d = date.split('/');
-    return (d[2]+'-'+d[1]+'-'+d[0]);
+    return (d[2] + '-' + d[1] + '-' + d[0]);
 }
